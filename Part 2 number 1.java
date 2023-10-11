ae8c8759b88b42fa0e3410f18bd0048fa8ddac96
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-10-10 20:59:56 -0400

    Create Part 2 number 1.java

---
+++ Part 2 number 1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the region that you're from: ");
+    
+    String data1 = input.nextLine();
+    
+    if (data1.equals("Toronto")) {
+      System.out.println("CN Tower, Rogers Center, and Royal Ontario Museum are the places that you might want to visit in Toronto");
+    } 
+    else if (data1.equals("NorthYork")) {
+      System.out.println("Ontario Science Centre, Toronto Botanical Garden, and Young Street are the places that you might want to visit in North York.");
+    } 
+    else if (data1.equals("Etobicoke")) {
+      System.out.println("Centennial Park, Marie Curtis Park, and Centennial Park Conservatory are the places that you might want to visit in Etobicoke.");
+    } 
+    else if (data1.equals("Niagara")) {
+      System.out.println("Niagara Falls, Niagara SkyWheel, and Skylon Tower are the places that you might want to visit in Niagara.");
+    } 
+    else {
+      System.out.println("I don't know anything about the region");
+    }
+  }
+}
