e07724c340e1148a7ca8a5a18a4612150b33844a
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-10-12 13:53:27 -0400

    Update Part 2 number 5.java

--- Part 2 number 5.java
+++ Part 2 number 5.java
@@ -1,28 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner; //initialization for Scanner
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); //declaring Scanner
 
-        System.out.println("play Rock, Paper, Scissors");
-        System.out.println("Pick what you want to choose for player 1");
-        System.out.println("1. Rock");
-        System.out.println("2. Paper");
-        System.out.println("3. Scissors");
-        int p1 = input.nextInt();
+        System.out.println("play Rock, Paper, Scissors"); 
+        System.out.println("Pick what you want to choose for player 1");// user input the value
+        System.out.println("1. Rock"); //option 1 for player 1
+        System.out.println("2. Paper");//option 2 for player 1
+        System.out.println("3. Scissors");//option 3 for player 1
+        int p1 = input.nextInt(); // declaring player 1
 
         System.out.println("Pick what you want to choose for player 2");
-        System.out.println("1. Rock");
-        System.out.println("2. Paper");
-        System.out.println("3. Scissors");
-        int p2 = input.nextInt();
+        System.out.println("1. Rock");//option 1 for player 2
+        System.out.println("2. Paper");//option 2 for player 2
+        System.out.println("3. Scissors");//option 3 for player 2
+        int p2 = input.nextInt(); // declaring player 2
 
-        if (p1 == p2) {
-            System.out.println("It's a tie!");
+        if (p1 == p2) { //if statement if the p1 is equal to p2
+            System.out.println("It's a tie!"); // if it is the result is tie
         } 
-        else if (p1 == 1 && p2 == 3 || p1 == 2 && p2 == 1 || p1 == 3 && p2 == 2) {
-            System.out.println("Player 1 wins!");
+        else if (p1 == 1 && p2 == 3 || p1 == 2 && p2 == 1 || p1 == 3 && p2 == 2) {  // papper win against rock, rock wins against scissors, and scissors wins against papper
+            System.out.println("Player 1 wins!"); 
         } 
-        else {
+        else { 
             System.out.println("Player 2 wins!");
         }
     }
