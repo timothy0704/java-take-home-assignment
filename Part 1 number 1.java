562d9762885e917a400a1e97bf12001fed620575
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-10-12 13:01:21 -0400

    Update Part 1 number 1.java

--- Part 1 number 1.java
+++ Part 1 number 1.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
 
-    Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in); // input 
     System.out.print("Enter the first name: ");
-    String data1 = input.nextLine();
+    String data1 = input.nextLine(); //initialization and declaration
     System.out.print("Enter the second name: ");
-    String data2 = input.nextLine();
+    String data2 = input.nextLine(); //initialization and declaration
 
     
 
-    if(data1.equals (data2)){
+    if(data1.equals (data2)){   //if statement
       System.out.println("they are the same");
     }
-    else{
+    else{  
       System.out.println("they are different");
     }
-    input.close();
+    input.close(); //closing for the input
   }
 }
