2673e3a6f2fc48c9c336c08965676b7a9678aa88
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-10-12 13:33:49 -0400

    Update Part 2 number 2.java

--- Part 2 number 2.java
+++ Part 2 number 2.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
+import java.util.Scanner; //Initializing Scanner
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // delcaring Scanner
 
-        System.out.print("Enter three numbers:");
-        double num1 = input.nextDouble();
-        double num2 = input.nextDouble();
-        double num3 = input.nextDouble();
+        System.out.print("Enter three numbers:"); // get the input from the user
+        double num1 = input.nextDouble(); //delcaring num 1
+        double num2 = input.nextDouble(); //delcaring num 2
+        double num3 = input.nextDouble(); //delcaring num 3
 
-        if (num1 <= num2 && num1 <= num3) {
+        if (num1 <= num2 && num1 <= num3) { // if statement 
             System.out.println("The smallest number: " + num1);
         } 
           else if (num2 <= num1 && num2 <= num3) {
