3156f0a52f2ee471bb1613e63cae804a2c086854
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-10-12 13:37:12 -0400

    Update Part 2 number 3.java

--- Part 2 number 3.java
+++ Part 2 number 3.java
@@ -1,18 +1,18 @@
-import java.util.Scanner;
+import java.util.Scanner; // initializing Scanner
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // declaring Scanner
 
-        System.out.print("Enter three numbers:");
-        double num1 = input.nextDouble();
-        double num2 = input.nextDouble();
-        double num3 = input.nextDouble();
+        System.out.print("Enter three numbers:"); // get the input from the user
+        double num1 = input.nextDouble(); // declaring num1
+        double num2 = input.nextDouble(); // declaring num2
+        double num3 = input.nextDouble(); // declaring num3
 
-        if (num1 >= num2 && num1 >= num3) {
+        if (num1 >= num2 && num1 >= num3) { //if statement
             System.out.println("The greatest number is: " + num1);
         } 
-          else if (num2 >= num1 && num2 >= num3) {
+          else if (num2 >= num1 && num2 >= num3) { 
               System.out.println("The greatest number is: " + num2);
           } 
             else {
