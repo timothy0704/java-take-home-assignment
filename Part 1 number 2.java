146d70bc48e76ec7aa25f9261eaa8b414c1cc4ac
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-10-12 13:10:14 -0400

    Update Part 1 number 2.java

--- Part 1 number 2.java
+++ Part 1 number 2.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-     Scanner input = new Scanner(System.in);
-    System.out.print("Enter the first number: ");
-    int num1 = input.nextInt();
-    System.out.print("Enter the second number: ");
+     Scanner input = new Scanner(System.in); //declare scanner
+    System.out.print("Enter the first number: "); //ask user a question
+    int num1 = input.nextInt(); //
+    System.out.print("Enter the second number: "); //ask user a question
     int num2 = input.nextInt();
 
-    if(num1 > num2) {
+    if(num1 > num2) { //if statement 
       System.out.println("The first number is greater than the second number");
     }
-      else if(num1 < num2) {
+      else if(num1 < num2) { //else if statement
       System.out.println("The second number is greater than the first number");
       }
-      else{
+      else{ // if the input does not match if or else if, the out put will produce what is in else
       System.out.println("The numbers are euqal");
       }
     }
